7321701110236c2a730ce3f00f8e1bad3b128247
Patrick Scheper <patrick_scheper@live.nl>
2019-09-04 15:35:52 +0200

    Added code

---
+++ VirtualButtonEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+
+[RequireComponent(typeof(VirtualButtonBehaviour))]
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    private VirtualButtonBehaviour buttonBehaviour;
+
+    public UnityEvent buttonPressedEvent;
+    public UnityEvent buttonReleasedEvent;
+    void Start()
+    {
+        buttonBehaviour = GetComponent<VirtualButtonBehaviour>();
+        buttonBehaviour.RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        buttonPressedEvent?.Invoke();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        buttonReleasedEvent?.Invoke();
+    }
+}
